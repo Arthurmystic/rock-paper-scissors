a161ca1c2be3f2221a5365b8abe532756ec67ffb
Arthur Mugoya <188759832+Arthurmystic@users.noreply.github.com>
2024-12-22 11:35:40 +1030

    Add playRound function and scoring system
    - Declared `humanScore` and `computerScore` variables to track player scores.
    - Implemented `playRound` function to compare human and computer choices.
      - Added logic for all possible outcomes: win, lose, or draw.
      - Updated scores based on the result.
    - Logged the result of `playRound` to the console for testing.
    - Included an alternative commented-out implementation using an object and switch-case - seems to not work as required thi

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ let getComputerChoice = () => randNum < 3 ?
     randNum < 6 ?
         "Paper" :
         "Scissor"
-console.log(getComputerChoice())
+// //console.log(getComputerChoice())
 
 // function getComputerChoice() {
 //     randNum = Math.random() * 10;
@@ -35,6 +35,95 @@ let gethumanChoice =function(){
         return gethumanChoice();
     }
 }
-console.log(gethumanChoice())
+// console.log(gethumanChoice())
+
+// Declare player's score
+let humanScore = 0;
+let computerScore = 0; 
+
+let playRound = function (humanChoice, computerChoice){
+    if (humanChoice == "Rock" && computerChoice == "Rock"){
+        return "It's a draw! Both rock.";
+    }else if (humanChoice == "Paper" && computerChoice == "Paper"){
+        return "It's a draw! Both Paper.";
+    }else if (humanChoice == "Scissor" && computerChoice == "Scissor"){
+        return "It's a draw! Both Scissor."
+    }else if (humanChoice == "Paper" && computerChoice == "Rock"){
+        humanScore +=1;
+        return "You win! Paper beats Rock."
+    }else if (humanChoice == "Rock" && computerChoice == "Paper"){
+        computerScore += 1;
+        return "You lose! Paper beats Rock."
+    }else if (humanChoice == "Scissor" && computerChoice == "Rock"){
+        computerScore += 1;
+        return "You lose! Rock beats Scissor"   
+    }else if (humanChoice == "Rock" && computerChoice == "Scissor"){
+        humanScore += 1;
+        return "You win! Rock beats Scissor."
+    }else if (humanChoice == "Paper" && computerChoice == "Scissor"){
+        computerScore += 1;
+        return "You lose! Scissor beats Paper."
+    }else if (humanChoice == "Scissor" && computerChoice == "Paper"){
+        humanScore += 1;
+        return "You win! Scissor beats Paper."
+    } 
+}
 
 
+// let playRound = function (humanChoice, computerChoice){
+//     const choices = {
+//         bothRock: () => humanChoice == "Rock" && computerChoice == "Rock",
+//         bothPaper: () => humanChoice == "Paper" && computerChoice == "Paper",
+//         bothScissor: () => humanChoice == "Scissor" && computerChoice == "Scissor",
+       
+//         PaperRock: () => humanChoice == "Paper" && computerChoice == "Rock",
+//         RockPaper: () => humanChoice == "Rock" && computerChoice == "Paper",
+       
+//         ScissorRock: () => humanChoice == "Scissor" && computerChoice == "Rock",
+//         RockScissor: () => humanChoice == "Rock" && computerChoice == "Scissor",
+        
+//         PaperScissor: () => humanChoice == "Paper" && computerChoice == "Scissor",
+//         ScissorPaper: () => humanChoice == "Scissor" && computerChoice == "Paper"
+//     }
+    
+//     switch (choices){
+//         case choices.bothRock():
+//             return "It's a draw! Both rock."
+//             break;
+//         case choices.bothPaper():
+//             return "It's a draw! Both Paper."
+//             break;
+//         case choices.bothScissor():
+//             return "It's a draw! Both Scissor."
+//             break;
+//         case choices.PaperRock():
+//             humanScore +=1;
+//             return "You win! Paper beats Rock."
+//             break;
+//         case choices.RockPaper():
+//             computerScore += 1;
+//             return "You lose! Paper beats Rock."
+//             break;
+//         case choices.ScissorRock():
+//             computerScore += 1;
+//             return "You lose! Rock beats Scissor"
+//             break;
+//         case choices.RockScissor():
+//             humanScore += 1;
+//             return "You win! Rock beats Scissor."
+//             break;
+//         case choices.PaperScissor():
+//             computerScore += 1;
+//             return "You lose! Scissor beats Paper."
+//             break;
+//         case choices.ScissorPaper():
+//             humanScore += 1;
+//             return "You win! Scissor beats Paper."
+//             break;
+        
+//     }
+
+// }
+
+// playRound(gethumanChoice(), getComputerChoice())
+console.log(playRound(gethumanChoice(), getComputerChoice()))
